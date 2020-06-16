1277ff98b424648b2d57e55e917210783a402832
Iaroslav Mokroguz <maustrauk@gmail.com>
2020-06-16 18:00:46 -0400

    06/16/20

--- mars.js
+++ mars.js
@@ -1,8 +1,7 @@
 let sol=1000;
-let camera='fhaz';
+let camera='NAVCAM';
 
-apiurl = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?' + 'sol' + sol + '&camera' + camera + '&api_key=' + key;
+apiurl = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?' + 'sol=' + sol + '&camera=' + camera + '&api_key=' + key;
 
-//json_obj = JSON.parse(Get(apiurl));
-//document.getElementById("marsImg").src=json_obj.img_src;
-console.log(Get(apiurl));
+json_obj = JSON.parse(Get(apiurl));
+document.getElementById("marsImg").src=json_obj.photos["0"].img_src;
